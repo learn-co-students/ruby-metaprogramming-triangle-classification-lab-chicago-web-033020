1551eb61302af25842edcf0fb61874460645f750
gozmeister <samgorick.sg@gmail.com>
2020-04-08 17:59:59 -0500

    Done.

--- lib/triangle.rb
+++ lib/triangle.rb
@@ -1,3 +1,29 @@
+require 'pry'
+
 class Triangle
-  # write code here
+  attr_reader :length_one, :length_two, :length_three
+  attr_accessor :equilateral, :isoceles, :scalene
+
+  def initialize (length_one, length_two, length_three)
+    @length_one = length_one
+    @length_two = length_two
+    @length_three = length_three
+  end
+
+  def kind
+    sum = length_one + length_two + length_three
+    arr = [length_one, length_two, length_three].sort
+    if sum <= 0 || arr[0] < 0 || arr[0] + arr[1] <= arr[2]
+        raise TriangleError
+    elsif length_one == length_two && length_two == length_three
+      :equilateral
+    elsif length_one != length_two && length_two != length_three && length_one != length_three
+      :scalene
+    else
+      :isosceles
+    end
+  end
+
+  class TriangleError < StandardError
+  end
 end
